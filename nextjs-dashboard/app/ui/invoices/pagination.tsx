755846a8c238b0c2f34a1b0717058beaa2950f34
Edwin Kambale <bambiedwins03@gmail.com>
2026-03-24 16:58:10 +0300

    latest changes

--- nextjs-dashboard/app/ui/invoices/pagination.tsx
+++ nextjs-dashboard/app/ui/invoices/pagination.tsx
@@ -6,48 +6,12 @@ import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
-  // NOTE: Uncomment this code in Chapter 10
-
-  // const allPages = generatePagination(currentPage, totalPages);
+  // TODO: to be uncommented  in Chapter 10
 
   return (
     <>
-      {/*  NOTE: Uncomment this code in Chapter 10 */}
+      {/*  TODO: TO BE erased in Chapter 10 */}
 
-      {/* <div className="inline-flex">
-        <PaginationArrow
-          direction="left"
-          href={createPageURL(currentPage - 1)}
-          isDisabled={currentPage <= 1}
-        />
-
-        <div className="flex -space-x-px">
-          {allPages.map((page, index) => {
-            let position: 'first' | 'last' | 'single' | 'middle' | undefined;
-
-            if (index === 0) position = 'first';
-            if (index === allPages.length - 1) position = 'last';
-            if (allPages.length === 1) position = 'single';
-            if (page === '...') position = 'middle';
-
-            return (
-              <PaginationNumber
-                key={`${page}-${index}`}
-                href={createPageURL(page)}
-                page={page}
-                position={position}
-                isActive={currentPage === page}
-              />
-            );
-          })}
-        </div>
-
-        <PaginationArrow
-          direction="right"
-          href={createPageURL(currentPage + 1)}
-          isDisabled={currentPage >= totalPages}
-        />
-      </div> */}
     </>
   );
 }
